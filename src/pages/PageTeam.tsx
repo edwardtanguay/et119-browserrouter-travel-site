af15593d94712d0f9fb123831fc3d75c93268dc0
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-21 11:22:35 +0100

    employee images in

--- src/pages/PageTeam.tsx
+++ src/pages/PageTeam.tsx
@@ -1,3 +1,24 @@
+import employees from "../data/employees.json";
+
 export const PageTeam = () => {
-	return <p>this is the team page</p>;
+	return (
+		<>
+			{employees.map((employee) => {
+				return (
+					<div className="flex mb-3 gap-3" key={employee.employeeID}>
+						<img
+							className="w-24 h-fit rounded"
+							src={`images/employees/employee_${employee.employeeID}.jpg`}
+						/>
+						<div>
+							<p className="font-semibold">
+								{employee.firstName} {employee.lastName}
+							</p>
+							<p className="text-xs">{employee.notes}</p>
+						</div>
+					</div>
+				);
+			})}
+		</>
+	);
 };
