b5c1e6552b7097acb57114e9b848bcd651a3e744
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-21 13:25:34 +0100

    image fixed with ../

--- src/components/Employee.tsx
+++ src/components/Employee.tsx
@@ -5,7 +5,7 @@ export const Employee = ({ employee, highlighted }: {employee: IEmployee, highli
 		<div className={`flex mb-3 gap-3 ${highlighted ? 'border-orange-500 border-4' : ''}`} key={employee.employeeID}>
 			<img
 				className="w-24 h-fit rounded"
-				src={`images/employees/employee_${employee.employeeID}.jpg`}
+				src={`../images/employees/employee_${employee.employeeID}.jpg`}
 			/>
 			<div>
 				<p className="font-semibold">
